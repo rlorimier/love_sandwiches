1c8b54dd924591d5dc9eeced275bac277ceafce5
rlorimier <rere.rnt@gmail.com>
2022-03-20 21:14:34 +0000

    add function to collect last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -112,6 +112,25 @@ def calculate_surplus_data(sales_row):
     #print(surplus_data)
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    #column = sales.col_values(3)
+    #print(column)
+
+    columns = []
+    for ind in range(1, 7):
+        #print(ind)
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    #pprint(columns)
+    return columns
+
 def main():
     """
     Run all program functions
